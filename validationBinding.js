8709421fa6ab0b3c6c43532caf55dc2ddbb01a98
Erik Pohl <epohl@dmigs.com>
2024-11-19 15:56:04 -0500

    initial commit for angular demo of more validation and binding

---
+++ validationBinding.js
@@ -0,0 +1,8 @@
+var app = angular.module("validationBinding",[]);
+
+app.controller("usersController", function($scope, $http) {
+	$http.get("https://jsonplaceholder.typicode.com/users")
+  .success(function(result) {
+  	$scope.users = result;
+  });
+});
